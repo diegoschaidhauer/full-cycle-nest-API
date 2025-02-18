7638959673f1fc9785f0f26bdf3c2b7e7ea4097c
diegoschaidhauer <xaxim84@gmail.com>
2025-02-18 15:31:21 -0300

    wallet-crud

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,34 @@
-export class Asset {}
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import crypto from 'crypto'
+import { HydratedDocument } from 'mongoose';
+
+
+export type AssestDocument = HydratedDocument<Asset>;
+
+@Schema({timestamps:true})
+export class Asset {
+ @Prop({default: () => crypto.randomUUID()})
+  _id: string;
+
+  @Prop()
+  name:string;
+
+  @Prop({unique:true, index:true})
+  symbo: string;
+
+  @Prop()
+  image:string;
+
+  @Prop()
+  price:string;
+
+  @Prop()
+  createdAt!: Date;
+
+  @Prop()
+  updatedAt!: Date;
+
+}
+
+export const AsserSchema = SchemaFactory.createForClass(Asset)
